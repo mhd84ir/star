d4a355dc895d31139efe59dd947370ca719533f2
unknown <mhd84.ir@gmail.com>
2022-03-25 03:02:50 +0430

    stare

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+for (int i = 0; i < 6; i++)
+{
+    { for (int x =0 ; x <= i+1 ; x++)
+        {
+         Console.Write(" ");
+        }
+    }
+
+    for (int j = 5; j > i; j--)
+    {
+        System.Console.Write(" ");
+        System.Console.Write("*");
+
+    }
+    System.Console.WriteLine("");
+}
